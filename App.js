88277bd696103941ae52f5000205e3c51470a36a
Matt Butler <mattb-w@hotmail.co.uk>
2016-06-12 20:26:51 +0100

    initial commit, server working

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+      <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
+
+// const App = () => <h1>Hello Eggheads</h1>
+export default App
