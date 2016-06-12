d03293da93a83766a8efd9fb38e6f77a94576f76
Matt Butler <mattb-w@hotmail.co.uk>
2016-06-12 20:44:02 +0100

    update state from input

--- App.js
+++ App.js
@@ -2,24 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-
+  constructor(){
+    super();
+    this.state = {txt: "this is the state text"}
+  }
+  update(e){
+    this.setState({txt: e.target.value})
+  }
   render(){
-    let txt = this.props.txt
-    return <h1>{txt}</h1>
+    return (
+      <div>
+      <input type="text"
+      onChange={this.update.bind(this)} />
+      <h1>{this.state.txt}</h1>
+      </div>
+    );
   }
 }
 
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
 
-App.defaultProps ={
-  txt: 'this is the default'
-}
-ReactDOM.render(
-  <App cat={5}  />,
-  document.getElementById('app')
-);
-// const App = () => <h1>Hello Eggheads</h1>
-// export default App
+
+export default App
